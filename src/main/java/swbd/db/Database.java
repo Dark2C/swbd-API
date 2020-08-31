17307ffcbf7dccaa87674b9e226914c2a94b25b7
Dark2C <41321160+Dark2C@users.noreply.github.com>
2020-08-31 17:53:45 +0200

    Fixed multiple db connections bug

--- src/main/java/swbd/db/Database.java
+++ src/main/java/swbd/db/Database.java
@@ -9,13 +9,15 @@ import javax.naming.InitialContext;
  *
  */
 public class Database {
+	private static Connection connection = null;
 	public static Connection Get_Connection() throws Exception {
-		Context env = (Context) new InitialContext().lookup("java:comp/env");
-		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-		String connectionURL = (String) env.lookup("dbConn");
-		Connection connection = null;
-		connection = DriverManager.getConnection(connectionURL, (String) env.lookup("dbUser"),
+		if(null == connection) {
+			Context env = (Context) new InitialContext().lookup("java:comp/env");
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			String connectionURL = (String) env.lookup("dbConn");
+			connection = DriverManager.getConnection(connectionURL, (String) env.lookup("dbUser"),
 				(String) env.lookup("dbPass"));
+		}
 		return connection;
 	}
 }
